df601eccf8b259be8b550318a3f5a18984115e45
Oriel Shulman <orielsh7@gmail.com>
2022-06-02 21:39:40 +0300

    ?

--- constants.py
+++ constants.py
@@ -1,17 +1,27 @@
-from typing import Final
-
-
 TRAIN_FEATURES_PATH = r'resorces/origin_data/train.feats.csv'
 TEST_FEATURES_PATH = r'resorces/origin_data/test.feats.csv'
 TRAIN_LABELS_0_PATH = r'resorces/origin_data/train.labels.0.csv'
 TRAIN_LABELS_1_PATH = r'resorces/origin_data/train.labels.1.csv'
 DATA_SAVE_PATH = r'resorces/data/'
 
-TRAIN_PERCENTAGE: Final = .7
-TEST_PERCENTAGE: Final = .2
-DEV_PERCENTAGE: Final = .125
+TRAIN_PERCENTAGE = .7
+TEST_PERCENTAGE = .2
+DEV_PERCENTAGE = .125
 
-# TEST_PERCENTAGE: Final = 1 - TRAIN_PERCENTAGE
+FEATURES_NAMES = ['Unnamed: 0', ' Form Name', ' Hospital', 'User Name', 'אבחנה-Age',
+                  'אבחנה-Basic stage', 'אבחנה-Diagnosis date', 'אבחנה-Her2',
+                  'אבחנה-Histological diagnosis', 'אבחנה-Histopatological degree',
+                  'אבחנה-Ivi -Lymphovascular invasion', 'אבחנה-KI67 protein',
+                  'אבחנה-Lymphatic penetration', 'אבחנה-M -metastases mark (TNM)',
+                  'אבחנה-Margin Type', 'אבחנה-N -lymph nodes mark (TNM)',
+                  'אבחנה-Nodes exam', 'אבחנה-Positive nodes', 'אבחנה-Side', 'אבחנה-Stage',
+                  'אבחנה-Surgery date1', 'אבחנה-Surgery date2', 'אבחנה-Surgery date3',
+                  'אבחנה-Surgery name1', 'אבחנה-Surgery name2', 'אבחנה-Surgery name3',
+                  'אבחנה-Surgery sum', 'אבחנה-T -Tumor mark (TNM)', 'אבחנה-Tumor depth',
+                  'אבחנה-Tumor width', 'אבחנה-er', 'אבחנה-pr',
+                  'surgery before or after-Activity date',
+                  'surgery before or after-Actual activity',
+                  'id-hushed_internalpatientid']
 
 SPLIT_SEED = 1
 GENERAL_SEED = 0
